9d781aea0b2d92516d5e19d193bfe10cf1ffd5ca
Pepolim <37425326+Pepolim@users.noreply.github.com>
2024-10-29 09:19:08 +0000

    Update POO_NP_04_2.cpp

--- 2024_10_24_NP/POO_NP_04_2.cpp
+++ 2024_10_24_NP/POO_NP_04_2.cpp
@@ -3,10 +3,11 @@
 
 using namespace std;
 
+// Função para mudar de linha
 void mudaLinha() {
     cout << endl;
 }
-
+// Funçao para mostrar a data e hora de criaçao do programa
 void dataCriadoPor(const string& nome) {
     time_t dataHora;
     time(&dataHora);
@@ -18,11 +19,14 @@ void dataCriadoPor(const string& nome) {
 // Definir a classe
 class Pessoa {
 public:
+    //inicializaçao dos atributos
     string nome;
     int idade;
     float classificacao;
-    
+
+    //metodo onde diz se pode ou não se inscrever
     void podeFrequentar(){
+        //printar os atributos
         cout << "Nome: " << nome << endl;
         cout << "Idade: " << idade << endl;
         cout << "Classificação: " << classificacao << endl;
@@ -38,14 +42,21 @@ public:
     }
 }; // fim da classe, com o terminador ";"
 
+
+/**
+ * The main entry point of the program. This function takes command-line arguments and creates a 
+ * Pessoa object with the provided name, age, and classification. It then calls the podeFrequentar() 
+ * method on the Pessoa object to determine if the person can enroll.
+ */
 int main(int argc, char* argv[]) {
     if (argc != 4) {
         cout << "Uso: " << argv[0] << " <nome> <idade> <classificacao>" << endl;
         return 1; // Retorna código de erro se o número de argumentos estiver incorreto
     }
-
+    //função para mostrar a data e hora de criaçao do programa
     dataCriadoPor("Nuno Pepolim");
 
+    //instanciação do objeto
     Pessoa eu;
 
     // Pega os argumentos da linha de comando e converte os tipos necessários
@@ -54,6 +65,7 @@ int main(int argc, char* argv[]) {
     eu.classificacao = atof(argv[3]); // Classificação (conversão de string para float)
 
     mudaLinha();
+    // Chama o método podeFrequentar() no objeto eu
     eu.podeFrequentar();
     mudaLinha();
 
